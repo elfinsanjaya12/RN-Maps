99e9581448f616c163a8b77b4644250e63e2b266
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-08-06 17:02:45 +0700

    React Native Maps

---
+++ App.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { Dimensions, StyleSheet, View, Text, Button } from 'react-native';
+import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.771707;
+const LONGITUDE = -122.4053769;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const GOOGLE_MAPS_APIKEY = 'AIzaSyBDGp4_SEIrnulMwrT1dk5BSd4Q83y2wYY';
+
+class Example extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // AirBnB's Office, and Apple Park
+    this.state = {
+      // location: {
+      //   latitude: 0,
+      //   longitude: 0
+      // },
+      coordinates: [
+        {
+          latitude: 37.3317876,
+          longitude: -122.0054812,
+        },
+        {
+          latitude: 37.771707,
+          longitude: -122.4053769,
+        },
+      ],
+    };
+
+    this.mapView = null;
+  }
+
+  onMapPress = (e) => {
+    this.setState({
+      coordinates: [
+        ...this.state.coordinates,
+        e.nativeEvent.coordinate,
+      ],
+    });
+  }
+
+  getLocation = () => {
+    console.log("pance")
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position)
+        const location = JSON.stringify(position);
+        console.log(location)
+        this.setState({
+          location
+        });
+      },
+      error => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <MapView
+          initialRegion={{
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}
+          style={StyleSheet.absoluteFill}
+          ref={c => this.mapView = c}
+          onPress={this.onMapPress}
+        >
+          {this.state.coordinates.map((coordinate, index) =>
+            <MapView.Marker key={`coordinate_${index}`} coordinate={coordinate} />
+          )}
+          {(this.state.coordinates.length >= 2) && (
+            <MapViewDirections
+              origin={this.state.coordinates[0]}
+              waypoints={(this.state.coordinates.length > 2) ? this.state.coordinates.slice(1, -1) : null}
+              destination={this.state.coordinates[this.state.coordinates.length - 1]}
+              apikey={GOOGLE_MAPS_APIKEY}
+              strokeWidth={3}
+              strokeColor="hotpink"
+              optimizeWaypoints={true}
+              onStart={(params) => {
+                console.log(`Started routing between "${params.origin}" and "${params.destination}"`);
+              }}
+              onReady={result => {
+                console.log(`hallo`)
+                console.log(`Distance: ${result.distance} km`)
+                console.log(`Duration: ${result.duration} min.`)
+
+                this.mapView.fitToCoordinates(result.coordinates, {
+                  edgePadding: {
+                    right: (width / 20),
+                    bottom: (height / 20),
+                    left: (width / 20),
+                    top: (height / 20),
+                  }
+                });
+              }}
+              onError={(errorMessage) => {
+                // console.log('GOT AN ERROR');
+              }}
+            />
+          )}
+        </MapView>
+        <Button title="pance" onPress={this.getLocation}></Button>
+        <Text>Location: {this.state.location}</Text>
+      </View>
+    );
+  }
+}
+
+export default Example;
